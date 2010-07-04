fa179f74b72d1a176de1d6bb3262c4be99e2c76c
David A. Burgess <dburgess@kestrelsp.com>
2010-07-04 15:28:06 -0700

    Change license to AGPLv3.

--- public-trunk/Globals/Globals.cpp
+++ public-trunk/Globals/Globals.cpp
@@ -9,16 +9,16 @@
 * See the LEGAL file in the main directory for details.
 
     This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
+    it under the terms of the GNU Affero General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+    GNU Affero General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
+    You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 */
@@ -30,9 +30,10 @@
 
 const char* gOpenBTSWelcome =
 	//23456789123456789223456789323456789423456789523456789623456789723456789
-	"OpenBTS, Copyright 2008, 2009, 2010 Free Software Foundation, Inc.\n"
+	"OpenBTS, Copyright 2008-2010 Free Software Foundation, Inc.\n"
 	"Release " VERSION " built " __DATE__ "\n"
-	"\"OpenBTS\" is a trademark of Kestrel Signal Processing, Inc.\n"
+	"\"OpenBTS\" is a trademark of Kestrel Signal Processing, Inc.,\n"
+	"regsitered with the US Patent and Trademark Office.\n"
 	"\nContributors:\n"
 	"  Kestrel Signal Processing, Inc.:\n"
 	"    David Burgess, Harvind Samra, Raffi Sevlian, Roshan Baliga\n"
@@ -49,7 +50,10 @@ const char* gOpenBTSWelcome =
 	"\n"
 	"\nThis program comes with ABSOLUTELY NO WARRANTY.\n"
 	"\nThis is free software; you are welcome to redistribute it\n"
-	"under the terms of GPLv3.\n"
+	"under the terms of AGPLv3.\n"
+	"Please see the COPYING file in the source code for information\n"
+	"about the AGPLv3 license and recommended procedures for compliance\n"
+	"with the Affero requirements of that license.\n"
 	"\nUse of this software may be subject to other legal restrictions,\n"
 	"including patent licsensing and radio spectrum licensing.\n"
 	"All users of this software are expected to comply with applicable\n"
